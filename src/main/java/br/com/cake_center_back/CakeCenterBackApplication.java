384fa4bb39645bb277de69e382dac1330f48d371
Peterson Odair <petersomodair@gmail.com>
2026-01-19 12:24:11 -0300

    commit inicial

---
+++ src/main/java/br/com/cake_center_back/CakeCenterBackApplication.java
@@ -0,0 +1,13 @@
+package br.com.cake_center_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CakeCenterBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CakeCenterBackApplication.class, args);
+	}
+
+}
